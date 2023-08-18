d6426eda05e74d73138fa659bf5d04aef6ce04f9
Hope-Cube <117855454+Hope-Cube@users.noreply.github.com>
2023-08-18 16:42:39 +0200

    Add files via upload

---
+++ Menu/Menu/Menu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using static Colorful.Console;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menu
+{
+    internal class Menu
+    {
+        private string _name;
+        private Color _color;
+        private static List<Menu> _children = new List<Menu>();
+        private static int _child_index_selected;
+        public Menu(string name, Color color, List<Menu> children)
+        {
+            _name = name;
+            _color = color;
+            _children = children;
+        }
+        public Menu()
+        {
+            _name = "[]";
+            _color = Color.White;
+        }
+        public void Addchild(Menu child)
+        {
+            _children.Add(child);
+        }
+        public void SetColor(Color color)
+        {
+            _color = color;
+        }
+        static void Update()
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_child_index_selected == i)
+                {
+                    if (_children[_child_index_selected].Color.GetBrightness() <= 0.5)
+                    {
+
+                    }
+                    else
+                    {
+                        WriteLine(menu.Name, menu.Color);
+                    }
+                }
+            }
+        }
+        public void Stepper(ConsoleKey key)
+        {
+            Update();
+        }
+        public void Enter()
+        {
+
+        }
+        public string Name { get => _name; }
+        public Color Color { get => _color; set => _color = value; }
+        internal List<Menu> Children { get => _children; }
+    }
+}
---
+++ Menu/Menu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static Colorful.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Colorful;
+
+namespace Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Parent = null;
+        }
+    }
+}
